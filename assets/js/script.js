b43e3c6cde72284664df0802b021d74a324fd8c3
Ratiem <ratidzo.makoni@gmail.com>
2022-12-24 12:20:05 +0000

    Create function for rules of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,3 +113,18 @@ rulesButton.addEventListener("click", rules);
 nextButton.addEventListener("click", function () {
     nextQuestion(questionCounter, maxQuestion);
 });
+
+/*Content displayed when the rules button is clicked*/
+
+function rules() {
+    introArea.innerHTML =
+        `<h3>Rules</h3>
+    <ol id="rules">
+        <li>There are 12 questions</li>
+        <li>There is one possible answer, click one of the two possible answers</li>
+        <li>You will earn one point for each correct answer</li>
+        <li>Click next to proceed after each question</li>
+        </ol>`;
+    rulesButton.classList.add("hide");
+
+}
