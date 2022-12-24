6d44d5ad750f3fbe421bb3f9180a3da82a844786
Ratiem <ratidzo.makoni@gmail.com>
2022-12-24 12:08:30 +0000

    Add the quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+//Wait for the DOM to finish loading before running the game
+
+//List of Questions for the Quiz
+
+let questions = [{
+    question: "Europe is the most densely populated continent?",
+    answer1: "True",
+    answer2: "False",
+    correct: "True",
+},
+{
+    question: "10% of the world population lives in Europe?",
+    answer1: "True",
+    answer2: "False",
+    correct: "True",
+},
+{
+    question: "Amsterdam, The Netherlands is the largest container port in Europe?",
+    answer1: "True",
+    answer2: "False",
+    correct: "False",
+},
+{
+    question: "Barcelona is the capital city of Spain?",
+    answer1: "True",
+    answer2: "False",
+    correct: "False",
+},
+{
+    question: "Buda Castle is located in Prague?",
+    answer1: "True",
+    answer2: "False",
+    correct: "False",
+},
+{
+    question: "Istanbul, Turkey is the most populous city in Europe?",
+    answer1: "True",
+    answer2: "False",
+    correct: "True",
+},
+{
+    question: "24 countries are part of the European Union?",
+    answer1: "True",
+    answer2: "False",
+    correct: "False",
+},
+{
+    question: "You can find the EU headquarters in Berlin, Germany?",
+    answer1: "True",
+    answer2: "False",
+    correct: "False",
+},
+{
+    question: "Lisbon is the capital of Portugal?",
+    answer1: "True",
+    answer2: "False",
+    correct: "True",
+},
+{
+    question: "If you are flying to Charles de Gaulle, you are flying to Rome, Italy?",
+    answer1: "True",
+    answer2: "False",
+    correct: "False",
+},
+{
+    question: "The busiest airport in Germany is Frankfurt Airport?",
+    answer1: "True",
+    answer2: "False",
+    correct: "True",
+},
+{
+    question: "The Euro was invented in the year 2000?",
+    answer1: "True",
+    answer2: "False",
+    correct: "False",
+},
+];
+
